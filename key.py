83d8bbc54c1a0b00433008b622209a89c6d8eba2
sunouchi <t@sunouchi.jp>
2015-11-01 18:14:34 +0900

    フォルダ移動

---
+++ key.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+
+# Twitter Keys
+CONSUMER_KEY = '1or0MFJQl0Btdb9b07uIPxr84'
+CONSUMER_SECRET = 'rADLwoG4mzjukSTxBXulgcuK1xuP1vX6I18mPdetlrR32gqgsQ'
+ACCESS_TOKEN = '3280375135-NoaYMjqwGAaHNJeaX5Lo7rqxvbd0X3ZYIYEqqmZ'
+ACCESS_SECRET = 'MA96y1gBxbIh3pGlaKaIzXcASfuVqBys0vSlWBijNUXED'
